82e8d8b07610278d8fd41e3f1b7cd68ca53a196e
Gabor Kozar <id@gaborkozar.me>
2024-05-29 02:51:14 +0200

    Added notification e-mail template

--- src/notify.rs
+++ src/notify.rs
@@ -1,5 +1,7 @@
 use crate::{CmdContext, Result, SeriesDetailsChanges};
+use lettre::{message::header::ContentType, Message, SmtpTransport, Transport};
 
 pub fn send_email_notifications(ctx: &CmdContext, all_changes: Vec<SeriesDetailsChanges>) -> Result<()> {
+    
     Ok(())
 }
